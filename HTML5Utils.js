adb72ab57024f10a3622be1dac7a93c892f5c228
Jacky Jiang <jiangke@fusion-solutions.com.au>
2014-12-05 17:34:20 +1030

    in progress

--- HTML5Utils.js
+++ HTML5Utils.js
@@ -6,24 +6,40 @@ var HTML5Utils=(function(){
 	
 	var XHR=(function(){
 
-		function init(method,url)
+		function init(method,url,async)
 		{
+			if(typeof async=='undefined') async=true;
 			this.xhr=new XMLHttpRequest();
 			if("withCredentials" in xhr){
 				// Check if the XMLHttpRequest object has a "withCredentials" property.
 				// "withCredentials" only exists on XMLHTTPRequest2 objects.
-				xhr.open(method, url, true);
+				xhr.open(method,url,async);
 			}else if(typeof XDomainRequest != "undefined") {
 				// Otherwise, check if XDomainRequest.
 				// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-				xhr = new XDomainRequest();
-				xhr.open(method, url);
+				xhr=new XDomainRequest();
+				xhr.open(method,url);
 			}else{
 				// Otherwise, CORS is not supported by the browser.
-				xhr = null;
+				xhr=null;
 			}
-			return xhr;
+			if(!xhr) throw new Error('CORS XHR is not supported by the browser');
 		}
+		
+		init.prototype.responseType='';
+		init.prototype.setRequestHeader=function(header,value){
+			this.xhr.setRequestHeader(header,value);
+		};
+		init.prototype.getResponseHeader=function(header){
+			return this.xhr.getResponseHeader();
+		};
+		init.prototype.getAllResponseHeaders=function(header,value){
+			return this.xhr.getAllResponseHeaders();
+		};
+		init.prototype.send=function(){
+			
+		};
+		
 		
 	});
 	
